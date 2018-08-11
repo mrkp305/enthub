769fe40350304822985a8da3cadee65d5c4f6e09
Percival Rapha <percy305y@gmail.com>
2018-08-11 14:22:44 +0200

    registered utils app in project settings

--- ehub/settings.py
+++ ehub/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'authentication.apps.AuthenticationConfig',
     'account.apps.AccountConfig',
+    'utils.apps.UtilsConfig',
 ]
 
 MIDDLEWARE = [
