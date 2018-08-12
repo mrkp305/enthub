71b6b97ab6a646843279cf44c05399df9768a7e5
Percival Rapha <percy305y@gmail.com>
2018-08-12 16:33:24 +0200

    Added profile links to templates

--- account/views.py
+++ account/views.py
@@ -6,13 +6,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 # Create your views here.
+from .forms import Profile as ProfileForm
 
-class Profile(LoginRequiredMixin, UserPassesTestMixin, View):
+class Profile(LoginRequiredMixin, View):
     login_url = '/auth/'
     def get(self, request):
         template = 'main/account/profile.html'
         context = {
-
+            'ProfileForm':ProfileForm(),
         }
         return HttpResponse(render(request, template, context))
     
